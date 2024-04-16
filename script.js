90931e7f31d53ed1af69b297f73bb4cd914cefeb
javohirgvdev <gayratovjavohir90@gmail.com>
2024-04-16 19:05:08 +0500

    I added html and js

---
+++ script.js
@@ -0,0 +1,31 @@
+// const developer = {
+//     name: "Javohir",
+//     surname: "Gayratov",
+//     age: "15",
+//     hobby: "Football, IT",
+//     language: "English, Russian",
+//     knowledge: IT = {
+//         first: "html",
+//         second: "css",
+//         third: "javascript"
+//     },
+//     teach: function speak() {
+//         for (let i = 0; i < 20; i++) {
+//             console.log("Hello guys...");
+//         }
+//     }
+// }; 
+
+// developer.teach();
+
+let ism = prompt("Ismingizni kiriting...");
+let id = prompt("Space Idingizni kiriting...");
+let pass = prompt("Space Passwordingizni kiriting...");
+
+const somebody = {
+    name: ism,
+    spaceID: id,
+    spacePassword: pass
+};
+
+console.log(somebody);
